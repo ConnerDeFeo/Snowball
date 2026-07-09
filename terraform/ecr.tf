c0523948d1e7d2fe28515c67ea242d9fcad61bd6
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-09 19:32:40 -0400

    docker login for ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,11 @@
+# Login for ecr (powershell)
+# cmd /c "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 857360184083.dkr.ecr.us-east-2.amazonaws.com"
 locals {
     registries = ["orchestrator", "analysis-pipeline", "review-pipeline"]
 }
 
 resource "aws_ecr_repository" "snowball" {
-  for_each             = toset(locals.registries)
+  for_each             = toset(local.registries)
   name                 = "snowball/${each.value}"
   image_tag_mutability = "IMMUTABLE"
 
