901437bbe02bfe0e670b384d5f4477725f03dc52
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-15 15:49:01 -0400

    new image to be used

--- scripts/push_ecr.sh
+++ scripts/push_ecr.sh
@@ -5,3 +5,5 @@ repo_name=$2
 
 docker tag "$image_name:latest" "857360184083.dkr.ecr.us-east-2.amazonaws.com/snowball/$repo_name:$tag"
 docker push "857360184083.dkr.ecr.us-east-2.amazonaws.com/snowball/$repo_name:$tag"
+
+echo "Image Tag: $tag"
