57ad53a9ee6daa742cf505a9310566dbda168b1e
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-15 15:51:10 -0400

    ecr regulation changes on images

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -21,13 +21,12 @@ resource "aws_ecr_lifecycle_policy" "cleanup" {
   policy = jsonencode({
     rules = [{
       rulePriority = 1
-      description  = "Expire untagged images after 14 days"
-      selection = {
-        tagStatus   = "untagged"
-        countType   = "sinceImagePushed"
-        countUnit   = "days"
-        countNumber = 7
-      }
+      description  = "Keep only the last 10 tagged images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
       action = { type = "expire" }
     }]
   })
