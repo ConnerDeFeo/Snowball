9bfccc00ab1b90b6d27cef820ce86ba02ace5fa8
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-16 18:14:02 -0400

    ws handling comments

--- orchestrator/internal/api/websocket.go
+++ orchestrator/internal/api/websocket.go
@@ -11,6 +11,7 @@ var upgrader = websocket.Upgrader{}
 
 // Echoes back whatever the client sends, so you can see the connection working.
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection to websocket and gets connection stream
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("websocket upgrade failed:", err)
@@ -18,7 +19,9 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	// Loop until error
 	for {
+		// Fetch message type, message, and error from connection stream, blocking call
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
 			break
