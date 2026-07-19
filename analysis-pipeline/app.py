28c1ebe9dadf99ca30900c65acd24ffde1788c8d
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-19 18:19:36 -0400

    adding router

--- analysis-pipeline/app.py
+++ analysis-pipeline/app.py
@@ -1,12 +1,14 @@
 # app.py
 from fastapi import FastAPI
 from routes.document_retrieval import router as document_retrieval_router
+from routes.grading import router as grading_router
 from routes.health import router as health_router
 
 def create_app():
     app = FastAPI()
 
     app.include_router(document_retrieval_router)
+    app.include_router(grading_router)
     app.include_router(health_router)
 
     return app
