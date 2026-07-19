c951d6c5903ea598d8fe7741dd7e45bb57907767
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-19 09:43:24 -0400

    setting up the scaffolding

---
+++ analysis-pipeline/grading/grade_category.py
@@ -0,0 +1,4 @@
+from grading.types.GradedTimePeriod import GradedTimePeriod
+
+def grade_category() -> GradedTimePeriod:
+    return None
---
+++ analysis-pipeline/grading/grade_company.py
@@ -0,0 +1,4 @@
+from grading.types.GradedTimePeriod import GradedTimePeriod
+
+def grade_company() -> list[GradedTimePeriod]:
+    return None
