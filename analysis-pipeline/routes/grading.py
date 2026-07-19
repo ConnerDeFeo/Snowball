4d25a1a30ac7036862048e22e0a8b3e98e592845
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-19 14:37:20 -0400

    testing if this ends up working out

--- analysis-pipeline/routes/grading.py
+++ analysis-pipeline/routes/grading.py
@@ -2,6 +2,9 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from grading.enums.RubricCategory import RubricCategory
+from grading.grade_section import grade_section
+
 router = APIRouter()
 
 class GradeRequest(BaseModel):
@@ -11,3 +14,17 @@ class GradeRequest(BaseModel):
 @router.post("/grade_company/{tckr}")
 def grade(tckr: str, grade_request: GradeRequest):
     return {"status": "ok"}
+
+class GradeSectionRequest(BaseModel):
+    start_date: str
+    end_date: str
+    rubric_category: RubricCategory
+
+@router.post("/grade_section/{tckr}")
+def grade_section_route(tckr: str, grade_section_request: GradeSectionRequest):
+    return grade_section(
+        tckr,
+        grade_section_request.start_date,
+        grade_section_request.end_date,
+        grade_section_request.rubric_category,
+    )
