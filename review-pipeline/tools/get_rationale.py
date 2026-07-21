658129bac3f7438eb7f6ca5f55c652c40662ea50
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-21 18:42:15 -0400

    get rationale

---
+++ review-pipeline/tools/get_rationale.py
@@ -0,0 +1,5 @@
+from utils.dynamo import section_grades_table
+
+def get_rationale(tckr: str, start: str, end: str, rubric_category: str) -> dict | None:
+    category_period = f"{start}#{end}#{rubric_category}"
+    return section_grades_table.get(tckr, category_period)
