df51325e89ea54c80845837f903414449f98424c
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-22 18:52:59 -0400

    testing grade

--- scripts/tests/grade.sh
+++ scripts/tests/grade.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-{
-    sleep 1
-    echo '{"start_date": "2020-01-01", "end_date": "2025-12-30", "rubric_category":"revenue_durability"}'
-    sleep 2
-} | wscat -c ws://localhost:8080/grade_section/AAPL
+wscat -c ws://localhost:8080/grade_section/AAPL \
+  -x '{"start_date": "2020-01-01", "end_date": "2025-12-30", "rubric_category":"revenue_durability"}' \
+  -w 30
