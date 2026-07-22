3656f51fd8673950984dfba96d3a10c5f44fffc3
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-22 18:23:52 -0400

    adding some gaurd rails

--- review-pipeline/tools/fetch_rationale.py
+++ review-pipeline/tools/fetch_rationale.py
@@ -1,5 +1,6 @@
 from utils.dynamo import section_grades_table
 
 def fetch_rationale(tckr: str, start: str, end: str, rubric_category: str) -> dict | None:
+    rubric_category = rubric_category.lower()
     category_period = f"{start}#{end}#{rubric_category}"
     return section_grades_table.get(tckr, category_period)
