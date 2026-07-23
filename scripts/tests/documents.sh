88f8fecb82d997e5d767fe191bac62a3eed2234a
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-23 14:43:42 -0400

    update scripts

--- scripts/tests/documents.sh
+++ scripts/tests/documents.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-{
-  sleep 1
-  echo '{"start_year": 2020, "end_year": 2025}'
-  sleep 2
-} | wscat -c ws://localhost:8080/documents/AAPL
+wscat -c ws://localhost:8080/documents/AAPL \
+  -x '{"start_year": 2020, "end_year": 2025}' \
+  -w 60
