e0645c84fab5cce624e77683cce3c47adbf19ed2
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-23 18:37:09 -0400

    updating the scripts

--- scripts/tests/grade.sh
+++ scripts/tests/grade.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -N "http://localhost:8080/grade_section/AAPL?start_year=2020&end_year=2025&rubric_category=revenue_durability"
+curl -N "http://localhost:8080/grade_section/AAPL" \
+  -H "Content-Type: application/json" \
+  -d '{"start_year": 2020, "end_year": 2025, "rubric_category": "revenue_durability"}'
