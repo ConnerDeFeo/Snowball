feab6545e051872ecaf378e4e85bd55be56db1fe
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-28 11:08:00 -0400

    updating scripts

--- scripts/tests/grade.sh
+++ scripts/tests/grade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -N "http://localhost:8080/grade_section/AAPL?start_year=2020&end_year=2025&rubric_category=revenue_durability"
+curl -N "http://localhost:8080/grade_all/AAPL?start_year=2020&end_year=2025&rubric_category=revenue_durability"
