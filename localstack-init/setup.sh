d7f5c82d4c79f8459ce2fcb03641af09a31d7f63
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-29 11:53:09 -0400

    setting up the local dev env

---
+++ localstack-init/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+awslocal s3 mb s3://snowball-documents-bucket
+
+awslocal dynamodb create-table \
+  --table-name snowball_documents \
+  --attribute-definitions AttributeName=accession,AttributeType=S \
+  --key-schema AttributeName=accession,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST
+
+awslocal dynamodb create-table \
+  --table-name snowball_findings \
+  --attribute-definitions AttributeName=tckr,AttributeType=S AttributeName=finding_key,AttributeType=S \
+  --key-schema AttributeName=tckr,KeyType=HASH AttributeName=finding_key,KeyType=RANGE \
+  --billing-mode PAY_PER_REQUEST
+
+awslocal dynamodb create-table \
+  --table-name snowball_section_grades \
+  --attribute-definitions AttributeName=tckr,AttributeType=S AttributeName=category_period,AttributeType=S \
+  --key-schema AttributeName=tckr,KeyType=HASH AttributeName=category_period,KeyType=RANGE \
+  --billing-mode PAY_PER_REQUEST
+
+awslocal dynamodb create-table \
+  --table-name snowball_rubric_directions \
+  --attribute-definitions AttributeName=rubric_category,AttributeType=S AttributeName=sk,AttributeType=S \
+  --key-schema AttributeName=rubric_category,KeyType=HASH AttributeName=sk,KeyType=RANGE \
+  --billing-mode PAY_PER_REQUEST
