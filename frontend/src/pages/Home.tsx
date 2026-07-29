89bbfa3ad721a07cbe8c8ecc2b4966306450120e
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-29 11:43:30 -0400

    adding a frontend setup

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-white">test</h1>
+    </div>
+  )
+}
+
+export default Home
