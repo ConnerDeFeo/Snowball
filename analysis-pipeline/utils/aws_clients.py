cdbdf32885e27fab569b2f2455360d6a22d190a3
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-30 13:17:41 -0400

    removing localstack

--- analysis-pipeline/utils/aws_clients.py
+++ analysis-pipeline/utils/aws_clients.py
@@ -1,14 +1,11 @@
 # utils/aws_clients.py
-import os
 import boto3
 
-_LOCAL_ENDPOINT = os.getenv("AWS_ENDPOINT_LOCAL_URL")
-
 def dynamo_client():
-    return boto3.resource("dynamodb", endpoint_url=_LOCAL_ENDPOINT)
+    return boto3.resource("dynamodb")
 
 def s3_client():
-    return boto3.client("s3", endpoint_url=_LOCAL_ENDPOINT)
+    return boto3.client("s3")
 
 def bedrock_client():
     return boto3.client("bedrock-runtime")
